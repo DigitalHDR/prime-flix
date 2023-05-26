78158fefc29ee3faa50b1e905207b70cc4b499f7
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-05-26 11:35:49 -0300

    detalhes do filme pt2

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import './style.css'
+
+import api from '../../services/api'
 
 const Filme = () => {
+  const { id } = useParams()
+  const [filme, setFilme] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: '27445f9e3d02c2da7d94f4c8d25f985f',
+            language: 'pt-BR',
+          },
+        })
+        .then(response => {
+          setFilme(response.data)
+          setLoading(false)
+        })
+        .catch(() => {
+          console.log('Filme não encontrado')
+        })
+    }
+
+    loadFilme()
+
+    return () => {
+      console.log('COMPONENTE FOI DESMONTADO')
+    }
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h2>Carregando Detalhes do Filme...</h2>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <h1>Bem vindo a página detalhes do filme </h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average}</strong>
+
+      <div className='area-buttons'>
+        <button>Salvar</button>
+        <button>
+          <a href="#">
+            Treiler
+          </a>
+        </button>
+      </div>
     </div>
   )
 }
